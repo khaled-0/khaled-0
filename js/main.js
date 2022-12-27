c254c1865504e109527abb34456cee10a2ec2e79
Khaled <spkhalad@gmail.com>
2022-12-28 02:07:35 +0600

    Do more things.
    -Add navigation for worked projects
    -Add Git & Tux
    -Add More projects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 window.addEventListener("resize", function (event) {
   document.body.scrollTo(0, 0); //Fixes the weird overflow behaivour on screen resize;
+  showHideProjectsNavOnOverflow();
 });
+
+function showHideProjectsNavOnOverflow() {
+  if (isOverflown(document.querySelector(".worked-projects-container"))) {
+    document.querySelector(".projects-nav-left").style.display = "block";
+    document.querySelector(".projects-nav-right").style.display = "block";
+  }
+}
+
+function scrollToLeft(viewSelector, scrollMultiplier = 1) {
+  const view = document.querySelector(viewSelector);
+  view.scrollLeft -= 100 * scrollMultiplier;
+}
+
+function scrollToRight(viewSelector, scrollMultiplier = 1) {
+  const view = document.querySelector(viewSelector);
+  view.scrollLeft += 100 * scrollMultiplier;
+}
+
+function isOverflown(element) {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
+}
