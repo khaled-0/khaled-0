a2554538489c794535a7035caa337c240a125587
Khaled <spkhalad@gmail.com>
2023-01-03 03:00:38 +0600

    Fix "Update github calander api"

--- js/main.js
+++ js/main.js
@@ -70,3 +70,18 @@ function preloadImages(paths) {
     img.src = path;
   });
 }
+
+function loadScript(scriptUrl) {
+  const script = document.createElement("script");
+  script.src = scriptUrl;
+  document.body.appendChild(script);
+
+  return new Promise((res, rej) => {
+    script.onload = function () {
+      res();
+    };
+    script.onerror = function () {
+      rej();
+    };
+  });
+}
