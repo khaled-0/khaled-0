ffb64eb0ff68fa4450387e710f33024cc38757f9
Khaled <spkhalad@gmail.com>
2023-01-07 04:49:26 +0600

    Add typewritter intro description

---
+++ js/typewriter-intro.js
@@ -0,0 +1,45 @@
+const introDescList = [
+  "Welcome to my portfolio.",
+  "Hope you're doing great.",
+  "Have a nice day ahead.",
+  "Hoping for a bright future forward.",
+  "Technology is cool.",
+];
+
+var currentItemOnScreen = 0;
+const textView = document.getElementById("introWelcomeSubtitle");
+
+async function typeWriter() {
+  //Pick the next item and so on
+  var introDesc = introDescList[currentItemOnScreen++ % introDescList.length];
+  //Hehe boii
+  if (randomIntFromInterval(100, 999) == 420) {
+    introDesc = "amongussssssss";
+    currentItemOnScreen = 0;
+  }
+
+  textView.innerHTML = "";
+  for (i = 0; i < introDesc.length; i++) {
+    textView.innerHTML += introDesc.charAt(i);
+    await sleep(50);
+  }
+  setTimeout(typeWriter, 2500);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  typeWriter();
+  //Blink the cursor
+  setInterval(() => {
+    const introCursor = document.getElementById("introCursor");
+    if (introCursor.style.opacity == 0) introCursor.style.opacity = 1;
+    else introCursor.style.opacity = 0;
+  }, 500);
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
